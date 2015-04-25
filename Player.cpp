09c3f2e33e27420d7319f5993eb23835f8dd6a4e
develjacob <mcjacob61@gmail.com>
2015-04-24 18:42:00 -0800

    Added Player Class, needs Entity

---
+++ Player.cpp
@@ -0,0 +1,17 @@
+// CS 372 Project 3
+// The Platformer
+// Noah Betzen, William Fisher, Jacob McKenna
+// Dylan Tucker, William Showalter, Saira Walia, Adam Walters
+// Player.cpp
+
+#include "Player.h"
+
+Player::Player():_lives(3)
+{}
+
+unsigned int Player::getLives(){
+
+	return _lives;
+
+}
+
---
+++ Player.h
@@ -0,0 +1,21 @@
+// CS 372 Project 3
+// The Platformer
+// Noah Betzen, William Fisher, Jacob McKenna
+// Dylan Tucker, William Showalter, Saira Walia, Adam Walters
+// Player.h
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player {
+public:
+	Player();
+	unsigned int getLives();
+
+
+private:
+	unsigned int _lives;
+
+};
+
+#endif // Player_H
