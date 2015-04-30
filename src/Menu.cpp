f81169da770660bf542fe0f1257a64f2fe587810
Noah Betzen <nbetzen@hotmail.com>
2015-04-29 19:33:22 -0800

    Added new classes for overlays, menus, and user interfaces.

---
+++ src/Menu.cpp
@@ -0,0 +1,7 @@
+// CS 372 Project 3
+// The Platformer
+// Noah Betzen, William Fisher, Jacob McKenna
+// Dylan Tucker, William Showalter, Saira Walia, Adam Walters
+// Menu.cpp
+
+#include "Menu.h"
---
+++ src/Overlay.h
@@ -0,0 +1,20 @@
+// CS 372 Project 3
+// The Platformer
+// Noah Betzen, William Fisher, Jacob McKenna
+// Dylan Tucker, William Showalter, Saira Walia, Adam Walters
+// Overlay.h
+
+#ifndef OVERLAY_H
+#define OVERLAY_H
+
+class Overlay
+{
+public:
+  ~Overlay() = default;
+  Overlay();
+
+private:
+
+};
+
+#endif
