796903981151fd3c7d50025cf75d89bde3efbc6b
Will Fisher <will@gina.alaska.edu>
2015-05-04 18:03:21 -0800

    adding in random chance for vertical walls

--- src/RandomWall.cpp
+++ src/RandomWall.cpp
@@ -1,11 +1,20 @@
 #include "RandomWall.h"
 #include "RoomWall.h"
 #include <memory>
+#include <random>
 
 void RandomWalls::generateContent(Room * room)
 {
-  auto topWall = std::make_shared<RoomWall>(250, 50);
-  room->spawn(topWall, room->getRandomPosition());
+  std::random_device rd;
+  std::uniform_int_distribution<int> randDirection(0, 100);
+  std::shared_ptr<RoomWall> wall;
 
+  if (randDirection(rd) < 50)
+    wall = std::make_shared<RoomWall>(250, 50);
+  else
+    wall = std::make_shared<RoomWall>(50, 250);
+
+
+  room->spawn(wall, room->getRandomPosition());
   // room->spawn(wall, );
 }
